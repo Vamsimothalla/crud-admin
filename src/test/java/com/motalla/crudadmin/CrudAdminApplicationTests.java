460e539f5150e227d79e51efd21d1d099466502f
vamsimotalla <vamsi.mothalla@gmail.com>
2024-10-10 13:27:33 +0530

    initial commit

---
+++ src/test/java/com/motalla/crudadmin/CrudAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motalla.crudadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
